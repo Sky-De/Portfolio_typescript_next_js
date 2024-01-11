7deecfad566e17d1a495b4cb56d375ee6cc9b149
skyDe <sky_de1991@outlook.com>
2024-01-11 11:52:55 -0800

    final edit of quickIntro video

--- constants/videos.ts
+++ constants/videos.ts
@@ -1,2 +1,2 @@
 export const QI_VIDEO_URL: string =
-  "https://res.cloudinary.com/dxukz1zt5/video/upload/v1700577963/portfolio/xpsjucuadkodkagebztp.mp4";
+  "https://res.cloudinary.com/dxukz1zt5/video/upload/v1705001949/portfolio/ghnjqvhdmctcmzkevhql.mp4";
