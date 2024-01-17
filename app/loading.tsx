5f3b4d33abbb9209b65de01fbe9d8ecdbf2677fe
skyDe <sky_de1991@outlook.com>
2024-01-17 08:05:10 -0800

    pre loader moved from layout file

---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+import LoaderCube from "@/components/loader/LoaderCube";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <LoaderCube />;
+}
