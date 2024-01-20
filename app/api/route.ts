0f301eb06bb8fb61a5948dc6ae2c9da5fc71df61
skyDe <sky_de1991@outlook.com>
2024-01-20 08:44:03 -0800

    home route api removed / debug production

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,6 @@
-import { NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+// import { NextApiResponse } from "next";
+// import { NextResponse } from "next/server";
 
-export const GET = async (req: Request, res: NextApiResponse) => {
-  return NextResponse.json({ message: "welcome to my server!" });
-};
+// export const GET = async (req: Request, res: NextApiResponse) => {
+//   return NextResponse.json({ message: "welcome to my server!" });
+// };
