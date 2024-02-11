ad96169459c0aac8cdf7e86306fac4c678dbf455
skyDe <sky_de1991@outlook.com>
2024-02-11 11:48:17 -0800

    tests added to aboutItem

--- components/aboutItems/AboutItems.test.tsx
+++ components/aboutItems/AboutItems.test.tsx
@@ -1,10 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import AboutItems from "./AboutItems";
 
-test("H3 title must be in document", () => {
+test("web development journey H3 title render", () => {
   render(<AboutItems />);
-  const aboutTitle = screen.getByRole("heading", {
+
+  const aboutTitle1 = screen.getByRole("heading", {
     name: /web development journey/i,
   });
-  expect(aboutTitle).toBeInTheDocument();
+  expect(aboutTitle1).toBeInTheDocument();
+  expect(aboutTitle1.tagName).toBe("H3");
+});
+
+test("online work H3 title render", async () => {
+  render(<AboutItems />);
+
+  const aboutTitle2 = screen.getByRole("heading", { name: /online work/i });
+  expect(aboutTitle2).toBeInTheDocument();
+  expect(aboutTitle2.tagName).toBe("H3");
+});
+
+test("abilities H3 title render", () => {
+  render(<AboutItems />);
+
+  const aboutTitle3 = screen.getByRole("heading", { name: /abilities/i });
+  expect(aboutTitle3).toBeInTheDocument();
+  expect(aboutTitle3.tagName).toBe("H3");
 });
--- components/aboutItems/AboutItems.tsx
+++ components/aboutItems/AboutItems.tsx
@@ -5,9 +5,10 @@ import TextMarker from "../textMarker/TextMarker";
 import { AboutItemsData } from "@/constants/aboutItems";
 
 const AboutItems: FC = () => {
-  const [selectedItemIndex, setSelectedItemIndex] = useState<number>(0);
+  const [selectedItemIndex, setSelectedItemIndex] = useState<number>(1);
   return (
     <div className="h-[80vh] flex flex-col justify-evenly">
+      <p role="alert">{selectedItemIndex}</p>
       <div className="flex flex-col gap-2 items-center md:items-start">
         {AboutItemsData &&
           AboutItemsData.map((item, index) => (
@@ -26,9 +27,9 @@ const AboutItems: FC = () => {
       </div>
       <ul className="list-disc flex flex-col gap-[1px] md:gap-3 sm:my-1 lg:my-3 h-[40svh] tall:mt-8">
         {AboutItemsData[selectedItemIndex].desc.length > 0 &&
-          AboutItemsData[selectedItemIndex].desc.map((item) => (
+          AboutItemsData[selectedItemIndex].desc.map((itemText) => (
             <li className="border-b-2 border-dashed pb-2 mb-1" key={uuid()}>
-              <TextMarker text={item} />
+              <TextMarker text={itemText} />
             </li>
           ))}
       </ul>
