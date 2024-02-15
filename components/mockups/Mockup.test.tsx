9ff3a15abfb28f3a6bfc31ab2a1cb37418843944
skyDe <sky_de1991@outlook.com>
2024-02-15 10:52:43 -0800

    mockup.test.tsx added more tests

--- components/mockups/Mockup.test.tsx
+++ components/mockups/Mockup.test.tsx
@@ -1,14 +1,21 @@
 import Mockup from "./Mockup";
 import { MockupProps } from "@/types/allTypes";
-import { customRender, screen } from "@/test-utils";
+import { customRender, fireEvent, screen } from "@/test-utils";
+import { store } from "@/redux/store/store";
+import { closeImageModel } from "@/redux/features/models/modelSlice";
 
 const mockMockupProps: MockupProps = {
-  laptopCoverUrl: "url1",
-  mobileCoverUrl: "url2",
-  tableCoverUrl: "url3",
+  laptopCoverUrl: "laptop image url",
+  mobileCoverUrl: "mobile image url",
+  tableCoverUrl: "tablet mobile url",
 };
 
 describe("Mockup", () => {
+  // it closes model after each test
+  beforeEach(() => {
+    store.dispatch(closeImageModel());
+  });
+
   it("section should render", () => {
     customRender(<Mockup {...mockMockupProps} />);
     const sectionElement = screen.getByTestId("section");
@@ -25,17 +32,51 @@ describe("Mockup", () => {
 
   it("svg-tablet should render and has width=820px height=1180px", () => {
     customRender(<Mockup {...mockMockupProps} />);
-    const mobileSvgElement = screen.getByTestId("svg-tablet");
-    expect(mobileSvgElement).toBeInTheDocument();
-    expect(mobileSvgElement).toHaveAttribute("width", "820");
-    expect(mobileSvgElement).toHaveAttribute("height", "1180");
+    const tabletSvgElement = screen.getByTestId("svg-tablet");
+    expect(tabletSvgElement).toBeInTheDocument();
+    expect(tabletSvgElement).toHaveAttribute("width", "820");
+    expect(tabletSvgElement).toHaveAttribute("height", "1180");
   });
 
   it("svg-laptop should render and has width=1920px height=869px", () => {
     customRender(<Mockup {...mockMockupProps} />);
-    const mobileSvgElement = screen.getByTestId("svg-laptop");
-    expect(mobileSvgElement).toBeInTheDocument();
-    expect(mobileSvgElement).toHaveAttribute("width", "1920");
-    expect(mobileSvgElement).toHaveAttribute("height", "869");
+    const laptopSvgElement = screen.getByTestId("svg-laptop");
+    expect(laptopSvgElement).toBeInTheDocument();
+    expect(laptopSvgElement).toHaveAttribute("width", "1920");
+    expect(laptopSvgElement).toHaveAttribute("height", "869");
+  });
+
+  it("click on mobile svg must open ImageModel with relevent url", () => {
+    customRender(<Mockup {...mockMockupProps} />);
+    const mobileSvgElement = screen.getByTestId("svg-mobile");
+    expect(store.getState().models.imageModel.isOpen).toBe(false);
+    fireEvent.click(mobileSvgElement);
+    expect(store.getState().models.imageModel.isOpen).toBe(true);
+    expect(store.getState().models.imageModel.imageUrl).toBe(
+      mockMockupProps.mobileCoverUrl
+    );
+  });
+
+  it("click on tablet svg must open ImageModel with relevent url", () => {
+    customRender(<Mockup {...mockMockupProps} />);
+
+    const tabletSvgElement = screen.getByTestId("svg-tablet");
+    expect(store.getState().models.imageModel.isOpen).toBe(false);
+    fireEvent.click(tabletSvgElement);
+    expect(store.getState().models.imageModel.isOpen).toBe(true);
+    expect(store.getState().models.imageModel.imageUrl).toBe(
+      mockMockupProps.tableCoverUrl
+    );
+  });
+  it("click on tablet svg must open ImageModel with relevent url", () => {
+    customRender(<Mockup {...mockMockupProps} />);
+
+    const laptopSvgElement = screen.getByTestId("svg-laptop");
+    expect(store.getState().models.imageModel.isOpen).toBe(false);
+    fireEvent.click(laptopSvgElement);
+    expect(store.getState().models.imageModel.isOpen).toBe(true);
+    expect(store.getState().models.imageModel.imageUrl).toBe(
+      mockMockupProps.laptopCoverUrl
+    );
   });
 });
