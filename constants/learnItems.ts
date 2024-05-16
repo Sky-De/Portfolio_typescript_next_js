e7656a3ad570590bcd65579a0c7fa45377ae2144
skyDe <sky_de1991@outlook.com>
2024-05-15 23:30:35 -0800

    learn item updated

--- constants/learnItems.ts
+++ constants/learnItems.ts
@@ -2,26 +2,24 @@ export const MasterListData: string[] = [
   "React-native",
   "Three-js",
   "E2E-Test",
-  "C#",
-  "Rust",
   "Python",
   "SQL",
-  "Unity",
   "Docker",
   "Linux",
 ];
 export const DeepenListData: string[] = [
   "Tailwind",
+  "Testing",
+  "Next-js",
+  "Design-patterns",
   "Express-js",
   "Node-js",
-  "Testing",
   "Mongodb",
-  "Next-js",
 ];
 export const ExploreListData: string[] = [
   "JavaScript",
   "TypeScript",
-  "Redux",
-  "SCSS",
   "React-js",
+  "Redux-toolkit",
+  "SCSS",
 ];
